d4926e8f298858dbfbc253c02978484f9c5a95e9
Sébastien Doeraene <sjrdoeraene@gmail.com>
2025-09-14 12:11:21 +0200

    Use the `ZoneRailsLight` upstreamed in `railways`.

--- sources/Aiguillages.scala
+++ sources/Aiguillages.scala
@@ -12,16 +12,12 @@ object Aiguillages extends Module:
     val openGate = new OpenGate
     val closeGateButton = new CloseGateButton
     val openGateButton = new OpenGateButton
-
-    val zoneRailsLightCreator = new ZoneRailsLightCreator
   end createComponents
 
   def closedGate(using Universe): ClosedGate = myComponentByID("closedGate")
   def openGate(using Universe): OpenGate = myComponentByID("openGate")
   def closeGateButton(using Universe): CloseGateButton = myComponentByID("closeGateButton")
   def openGateButton(using Universe): OpenGateButton = myComponentByID("openGateButton")
-
-  def zoneRailsLightCreator(using Universe): ZoneRailsLightCreator = myComponentByID("zoneRailsLightCreator")
 end Aiguillages
 
 export Aiguillages.*
@@ -45,36 +41,3 @@ class OpenGateButton(using ComponentInit) extends PushButton:
     import context.*
     pos.map(10, 0, 0) = grass + openGate
 end OpenGateButton
-
-final class ZoneRailsLightCreator(using ComponentInit) extends ComponentCreator[ZoneRailsLight]:
-  category = ComponentCategory("rails", "Rails")
-
-  icon += "Rails/LightOffNorth"
-  icon += "Creators/Creator"
-end ZoneRailsLightCreator
-
-class ZoneRailsLight(using ComponentInit) extends RailsLight derives Reflector:
-  var zoneMap: Option[Map] = None
-  var zoneStart: Position = Position.Zero
-  var zoneEnd: Position = Position.Zero
-
-  @noinspect
-  val updateLightQueue = TimerQueue[Unit] { value =>
-    updateLight()
-  }
-
-  override def reflect() = autoReflect[ZoneRailsLight]
-
-  def updateLight(): Unit =
-    for zoneMap <- this.zoneMap do
-      val isZoneOccupied = (zoneStart to zoneEnd).exists { pos =>
-        zoneMap.posComponentsBottomUp(pos).exists(_.isInstanceOf[TrainPart])
-      }
-      isOn = !isZoneOccupied
-
-      updateLightQueue.schedule(250, ())
-  end updateLight
-
-  override def startGame(): Unit =
-    updateLight()
-end ZoneRailsLight
